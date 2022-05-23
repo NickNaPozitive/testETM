8f89d59fe9a77c2c128a1da455b756d11ca297c3
NickNaPoztive <korbofos.nn@gmail.com>
2022-05-23 17:44:03 +0300

    Сейчас будет com

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
-    String SITE_URL = "https://www.google.ru/";
+    String SITE_URL = "https://www.google.com/";
     public WebDriver driver = new ChromeDriver();
     public WebElement searchBox;
 
@@ -41,13 +41,13 @@ public class FirstTest {
 
     @Test
     public void testSearchGoogle(){
-//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-//        WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.setHeadless(false);
-//        ChromeDriver driver = new ChromeDriver(options);
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get(SITE_URL);
 
